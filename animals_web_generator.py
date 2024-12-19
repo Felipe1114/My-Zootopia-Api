8c7c76f290f1bc5344d4628eda4442aa5e795293
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-12-19 10:39:45 +0100

    api gelegt und projekt beendet

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,18 +1,40 @@
-import json
-JSON_DATAPATH = "animals_data.json"
+import requests
 HTML_DATAPATH = "animals_template.html"
+API_KEY = {"X-Api-Key": "rPicwarm61jK9uOZ+wNI3A==3bUhKh0YiLMZm4ag"}
 
-def main():
-  update_HTML_data()
+"""
+  - wenn ein name eingebuat wird, der nicht existert, soll eine "fehlermeldung" auf der website geprintet werden
+"""
+
+def create_request():
+  """creates the API request, on base of the user input"""
+  animal = input("what animal, you want information for?(animal name):")
+
+  url = "https://api.api-ninjas.com/v1/animals"
+  get_request = f"?name={animal}"
+
+  full_request = url + get_request
+
+  return full_request, animal
 
 
-def get_json_data():
-  with open(JSON_DATAPATH, "r") as objfile:
-    data = json.load(objfile)
+def get_response():
+  """gets data from api"""
+  full_request, animal = create_request()
+
+  response = requests.get(full_request, params=API_KEY)
+
+  return response, animal
+
+
+def get_json_from_request(response):
+  """changes response data, to json structure"""
+  data = response.json()
   return data
 
 
 def get_HTML_data():
+  """gets the data from the HTML file"""
   with open(HTML_DATAPATH, "r") as htmlfile:
     data = htmlfile.read()
 
@@ -20,15 +42,47 @@ def get_HTML_data():
 
 
 def save_HTML_data(data):
-  with open(HTML_DATAPATH, "w") as objfile:
-    objfile.write(data)
+  """Saves data to HTML file"""
+  try:
+    with open(HTML_DATAPATH, "w") as objfile:
+      objfile.write(data)
+    print("Website was successfully generated to the file animals.html.")
+  except FileNotFoundError as e:
+    print(e)
 
 
-def get_all_animal_data():
-  """Gets needed information, form json file and puts it - for each animal - in a list of lists
-  if animal has no 'type' key, tpye will is not appended to list
+def generate_new_HTML_code():
   """
-  data = get_json_data()
+  """
+  response, animal = get_response()
+  # hier wird der response code gecheckt, bevor er in eine json umgewandelt wird
+  output = validade_response(response, animal)
+
+  return output
+
+
+def validade_response(response, animal):
+  """Checks the status code. If status_code is 200, it gives back response,
+  if not, it gives back an error string
+
+  """
+  response_len = len(response.json())
+  if response_len > 0:
+    output = generate_animal_informations(response)
+
+  else:
+    output = (f"<li class='cards__item'>\n"
+              f"<div class='card__title'>{animal}</div>\n"
+              f"<pclass='card__text'>the animal {animal} does not exist</p>\n"
+              f"</li>\n")
+
+  return output
+
+
+
+def get_animal_informations(response):
+  data = get_json_from_request(response)
+
   all_animal_informations = []
   for animal in data:
     name = animal["name"]
@@ -43,13 +97,16 @@ def get_all_animal_data():
     except KeyError:
       animal_informations = [name, diet, location]
       all_animal_informations.append(animal_informations)
-
   return all_animal_informations
 
 
-def generate_animal_informations():
-  """Generates for each list of animal-information HTML-code and wirtes it into a HTML file"""
-  animal_informations = get_all_animal_data()
+def generate_animal_informations(response):
+  """Generates for each list of animal-information HTML-code and wirtes it into a HTML file
+
+  Args:
+    data(dict): contains all informations about animals of a specific type
+  """
+  animal_informations = get_animal_informations(response)
   output = ""
 
   for animal in animal_informations:
@@ -59,12 +116,14 @@ def generate_animal_informations():
     diet = animal[1]
     location = animal[2]
 
+    # text, if animal has a "type"
     if len(animal) == 4:
       type = animal[3]
       output += (f"<div class='card__title'>{name}</div>\n"
                  f"<p class='card__text'>\n<strong>Diet:</strong> {diet}<br/>\n"
                  f"<strong>Location:</strong> {location}<br/>\n<strong>Type:</strong> {type}<br/>"
                  f"\n</p>\n</li>\n")
+    # text, if animal has no "type"
     else:
       output += (f"<div class='card__title'>{name}</div>\n"
                  f"<p class='card__text'>\n<strong>Diet:</strong> {diet}<br/>\n"
@@ -76,11 +135,15 @@ def generate_animal_informations():
 
 def update_HTML_data(old_data="__REPLACE_ANIMALS_INFO__"):
   html = get_HTML_data()
-  animal_info = generate_animal_informations()
+  new_html_data = generate_new_HTML_code()
 
-  html = html.replace(old_data, animal_info)
+  html = html.replace(old_data, new_html_data)
 
   save_HTML_data(html)
+
+
+def main():
+  update_HTML_data()
 
 
 if __name__ == "__main__":
